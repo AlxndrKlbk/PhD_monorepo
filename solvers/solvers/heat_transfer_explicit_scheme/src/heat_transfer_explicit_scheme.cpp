fb2faad357da205d2bc60749c4363ebe5f10dde3
abkolbeko <sasha-kolbeko@mail.ru>
2024-09-23 11:15:12 +0500

    usage of number fronted with cpp_bin_float templated backend

--- solvers/solvers/heat_transfer_explicit_scheme/src/heat_transfer_explicit_scheme.cpp
+++ solvers/solvers/heat_transfer_explicit_scheme/src/heat_transfer_explicit_scheme.cpp
@@ -1,16 +1,18 @@
-#include <boost/multiprecision/float128.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
 #include <boost/math/special_functions/gamma.hpp>
 #include <iostream>
 
 
-// using boost::multiprecision::float128;
+namespace bmp = boost::multiprecision;
+using boost_float = bmp::number<bmp::cpp_bin_float<200>>;
 
 int main(int argc, char ** argv)
 {
-    using namespace boost::multiprecision;
-    float128 quad_float = 2.1;
+    boost_float f1 = 2.1;
+    boost_float f2 = 3.2;
     std::cout << argc << argv << std::endl;
-    std::cout << boost::math::tgamma(quad_float) << std::endl;
+    std::cout << f1 + f2 << std::endl;
+    std::cout << boost::math::tgamma(f1) << std::endl;
     //call module in function.hpp
     return 0;
 }
