aed1e4060aa22e2bd2e228171a0c013da9311dcc
abkolbeko <sasha-kolbeko@mail.ru>
2024-10-01 22:57:22 +0500

    added image with equation

--- solvers/solvers/heat_transfer_explicit_scheme/src/heat_transfer_explicit_scheme.cpp
+++ solvers/solvers/heat_transfer_explicit_scheme/src/heat_transfer_explicit_scheme.cpp
@@ -16,19 +16,19 @@ using boost_float = bmp::number<bmp::cpp_bin_float<200>>;
 const boost_float a = 1;               // коэффициент температуропроводности a = теплопроводность / (теплоемкость * плотность)
 const boost_float L = 100;             // длина моделируемой области (м)
 constexpr size_t x_steps = 100;      // количество точек по пространству
-constexpr ulong T = 3600;            // период времени для моделирования (секунд)
+constexpr ulong t = 3600;            // период времени для моделирования (секунд)
 constexpr ulong tau_steps = 100;      // количество временных шагов
 
 // граничные условия
 // r {0;1}
 boost_float U_r_tau0 = 20;              // U(r, 0) начальное распределние температуры (C) в области
-boost_float U_r0_tau = 300;             // U(0, tau) - функция(const) температура на левой границе
+boost_float U_r0_tau = 400;             // U(0, tau) - функция(const) температура на левой границе
 boost_float U_r_tau = U_r_tau0;          // U(1, tau) - функция(const) температура на правой границе
 
 // таким образом
-constexpr ulong tau = T / tau_steps;                                            // шаг по времени в секундах
+constexpr ulong tau = t / tau_steps;                                            // шаг по времени в секундах
 const boost_float h = 1.0 / tau_steps;                                          // h параметр (приращение по безразмерной координате)
-const boost_float dtau = T / tau_steps * (bmp::pow(a, 2) / bmp::pow(L, 2));     // dtau (приращение по безразмерному времени)
+const boost_float dtau = t / tau_steps * (bmp::pow(a, 2) / bmp::pow(L, 2));     // dtau (приращение по безразмерному времени)
 
 using calculation_net = std::array<std::array<boost_float, tau_steps>, x_steps>;
 
@@ -67,6 +67,7 @@ int solve()
             auto&& u_prev = u[i-1][tau].str();
             auto&& u_next = u[i+1][tau].str();
             auto&& u_here = u[i][tau].str();
+            auto&& a2 = (a1 / h_square * dtau).str();
             // debug code end
             auto&& calculated = (((u[i+1][tau] - 2 * u[i][tau] + u[i-1][tau]) / h_square ) * dtau + u[i][tau]);
             auto const&& calc_val = calculated.str();
